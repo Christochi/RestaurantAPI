f8ca75ecf7945c361dc7c7c309c79faae377d334
Christochi <tochukwui2@gmail.com>
2023-05-28 05:01:17 +0000

    comments

--- websever/server.go
+++ websever/server.go
@@ -11,7 +11,7 @@ const addr string = ":3000"
 // starts server
 func RunServer() {
 
-	// the web files are processed relative to the dir of the caller (main.go) 
+	// the web files are processed relative to the dir of the caller 
 	fileHandler := http.FileServer(http.Dir("./static"))
 
 	// matches handler with incoming request (endpoint)
@@ -22,10 +22,11 @@ func RunServer() {
 
 }
 
+// handles browser caching
 func noCache(h http.Handler) http.Handler {
 
 	// the ResponseWriter is what the server will respond with or send the client
-	// Resquest is what the client sends to the server
+	// Request is what the client sends to the server
 	handlerFunc := func(w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate;")
 		w.Header().Set("pragma", "no-cache")
