3e4e5c14ff4c13567ec9392b586e84e1453e714b
Christochi <tochukwui2@gmail.com>
2023-05-29 03:40:27 +0000

    run server

--- api/chef/chef_test.go
+++ api/chef/chef_test.go
@@ -2,4 +2,16 @@ package chef
 
 import (
 	"testing"
+	"restaurantapi/webserver"
 )
+
+func TestChef(t *testing.T) {
+
+	// directory of web files
+	webserver.WebFilesDir = "../../static"
+	
+	// start server
+	webserver.RunServer(webserver.WebFilesDir)
+
+}
+
