55dae235e5317241dfef78efd4b13e742d7a91c4
Christochi <tochukwui2@gmail.com>
2023-05-29 06:35:10 +0000

    post request

--- api/chef/chef.go
+++ api/chef/chef.go
@@ -1,1 +1,27 @@
 package chef
+
+import (
+	"net/http"
+	"encoding/json"
+	"log"
+)
+
+type postBody struct {
+	code int `json:code`
+}
+
+func PostMethod(rw http.ResponseWriter, req *http.Request) int {
+
+	var post postBody 
+
+	if req.Method == "POST" {
+		err := json.NewDecoder(req.Body).Decode(&post)
+
+		if err != nil {
+			log.Fatal("error decoding into int")
+		}
+		return post.code
+	} else {
+		return 0
+	}
+}
