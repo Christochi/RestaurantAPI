6e21a62100fa19e0015c03023f5252047048440f
Christochi <tochukwui2@gmail.com>
2023-05-29 06:35:39 +0000

    test file

--- api/chef/chef_test.go
+++ api/chef/chef_test.go
@@ -3,15 +3,33 @@ package chef
 import (
 	"testing"
 	"restaurantapi/webserver"
+	"net/http"
 )
 
 func TestChef(t *testing.T) {
 
+	http.HandleFunc("/chef", func(rw http.ResponseWriter, req *http.Request) {
+		TestPostMethod(t)
+	})
+
 	// directory of web files
 	webserver.WebFilesDir = "../../static"
-	
+
 	// start server
 	webserver.RunServer(webserver.WebFilesDir)
 
+}
+
+func TestPostMethod(t *testing.T) {
+	 
+	var rw http.ResponseWriter
+	var req *http.Request
+
+	code := PostMethod(rw, req)
+	expected := 200
+
+	if code != expected {
+		t.Errorf("expected %v, got %v", expected, code)
+	}
 }
 
