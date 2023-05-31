0ad186abd44882c1c4bf7f42e752a4221c7367e9
Christochi <tochukwui2@gmail.com>
2023-05-31 02:44:42 +0000

    removed Filehandler as global var

--- webserver/server.go
+++ webserver/server.go
@@ -3,27 +3,30 @@ package webserver
 import (
 	"net/http"
 	"log"
+	"restaurantapi/api/chef"
 )
 
 // CONSTANTS
 const addr string = ":3000"
 
 var WebFilesDir string // directory of web files
-var FileHandler http.Handler // placeholder for web files
 
 // starts server
 func RunServer(dir string) {
 
 	// stores handler that serves requests with web files
-	FileHandler = http.FileServer(http.Dir(WebFilesDir))
+	fileHandler := http.FileServer(http.Dir(WebFilesDir))
 
 	// matches handler with incoming request (endpoint)
-	http.Handle("/", noCache(http.StripPrefix("/", FileHandler)))
+	http.Handle("/", noCache(http.StripPrefix("/", fileHandler)))
+	http.HandleFunc("/postchef", chef.PostMethod)
 
 	// listens on the network address and handles requests from incoming connections
 	log.Fatal(http.ListenAndServe(addr, nil))	
 
 }
+
+
 
 // handles browser caching
 func noCache(h http.Handler) http.Handler {
