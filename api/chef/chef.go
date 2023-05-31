631b5e74b5d97e45bda3f4505f7921188e96e928
Christochi <tochukwui2@gmail.com>
2023-05-31 03:05:35 +0000

    more comments

--- api/chef/chef.go
+++ api/chef/chef.go
@@ -14,18 +14,22 @@ type Chef struct {
 	About string `json:"about"`
 }
 
+// client send chef data using POST Method
 func PostMethod(rw http.ResponseWriter, req *http.Request) {
 
 	var chef []Chef // list of chefs
 
+	// decode json to struct
 	err := json.NewDecoder(req.Body).Decode(&chef)
 
+	// error handling
 	if err != nil {
 		log.Fatal("error decoding into struct")
 	}
 
-	fmt.Printf("%+v\n", p)
+	//fmt.Printf("%+v\n", p)
 	
+	// server's response to client
 	fmt.Fprintln(rw, "success")
 		
 	
