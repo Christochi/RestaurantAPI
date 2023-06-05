c9dd2b5ae907679d82da76531514632fcc03c942
Christochi <tochukwui2@gmail.com>
2023-06-05 15:40:39 +0000

    linting

--- api/chef/chef.go
+++ api/chef/chef.go
@@ -1,20 +1,19 @@
 package chef
 
 import (
-	"net/http"
 	"encoding/json"
-	"log"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 // Data about a chef
 type Chef struct {
-
-	Name string `json:"name"`
-	About string `json:"about"` 	
+	Name  string `json:"name"`
+	About string `json:"about"`
 }
 
-var chef []Chef // list of chefs 
+var chef []Chef // list of chefs
 
 // client send chef data using POST Method
 func PostMethod(rw http.ResponseWriter, req *http.Request) {
@@ -26,9 +25,9 @@ func PostMethod(rw http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Fatal("error decoding into struct")
 	}
-	
+
 	// server's response to client
-	fmt.Fprintln(rw, "success")	
+	fmt.Fprintln(rw, "success")
 }
 
 // client requests for chef data using GET Method
