9b4e09def25601143d2aea34db10ce3c026782f6
Christochi <tochukwui2@gmail.com>
2023-06-05 15:28:30 +0000

    linting

--- webserver/server.go
+++ webserver/server.go
@@ -1,8 +1,8 @@
 package webserver
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 	"restaurantapi/api/chef"
 )
 
@@ -23,7 +23,7 @@ func RunServer(dir string) {
 	http.HandleFunc("/getchef", chef.GetMethod)
 
 	// listens on the network address and handles requests from incoming connections
-	log.Fatal(http.ListenAndServe(addr, nil))	
+	log.Fatal(http.ListenAndServe(addr, nil))
 
 }
 
@@ -32,12 +32,12 @@ func noCache(h http.Handler) http.Handler {
 
 	// the ResponseWriter is what the server will respond with or send the client
 	// Request is what the client sends to the server
-	handlerFunc := func(w http.ResponseWriter, r *http.Request){
+	handlerFunc := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate;")
 		w.Header().Set("pragma", "no-cache")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 
-		h.ServeHTTP(w,r)
+		h.ServeHTTP(w, r)
 	}
 
 	return http.HandlerFunc(handlerFunc)
