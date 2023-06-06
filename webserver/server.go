cc9c583a4048549220e015e7ff1be4e1468415dc
Christochi <tochukwui2@gmail.com>
2023-06-06 18:35:05 +0000

    changesd all occurences of w and r to rw and req

--- webserver/server.go
+++ webserver/server.go
@@ -34,12 +34,12 @@ func noCache(h http.Handler) http.Handler {
 
 	// the ResponseWriter is what the server will respond with or send the client
 	// Request is what the client sends to the server
-	handlerFunc := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate;")
-		w.Header().Set("pragma", "no-cache")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
+	handlerFunc := func(rw http.ResponseWriter, req *http.Request) {
+		rw.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate;")
+		rw.Header().Set("pragma", "no-cache")
+		rw.Header().Set("X-Content-Type-Options", "nosniff")
 
-		h.ServeHTTP(w, r)
+		h.ServeHTTP(rw, req)
 	}
 
 	return http.HandlerFunc(handlerFunc)
