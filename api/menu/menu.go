e88c6713b93df0fdcbc67d76082699b27add278c
Christochi <tochukwui2@gmail.com>
2023-06-14 19:03:22 +0000

    switch case for GetDrinksMenu

--- api/menu/menu.go
+++ api/menu/menu.go
@@ -59,6 +59,9 @@ func (m *menu) MenuHandler(rw http.ResponseWriter, req *http.Request) {
 	case req.Method == http.MethodGet && allDinnerRegex.MatchString(req.URL.Path):
 		m.GetDinnerMenu(rw, req)
 
+	case req.Method == http.MethodGet && allDrinksRegex.MatchString(req.URL.Path):
+		m.GetDrinksMenu(rw, req)
+
 	case req.Method == http.MethodDelete && allMenuRegex.MatchString(req.URL.Path):
 		m.DeleteMenu(rw, req)
 
