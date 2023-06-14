e44146925111294d2fc056b877a8c4241fc00710
Christochi <tochukwui2@gmail.com>
2023-06-14 17:06:24 +0000

    MenuHandler implementation

--- api/menu/menu.go
+++ api/menu/menu.go
@@ -1,6 +1,7 @@
 package menu
 
 import (
+	"net/http"
 	"regexp"
 )
 
@@ -23,5 +24,35 @@ type menu []menuJson // slice type to be used as a receiver for methods
 func NewChef() *menu {
 
 	return new(menu)
+
+}
+
+// handlerfunc for menu endpoint
+func (c *menu) MenuHandler(rw http.ResponseWriter, req *http.Request) {
+
+	// inform browser to expect json
+	rw.Header().Set("Content-Type", "application/json")
+
+	// determines the function to call by the request
+	switch {
+
+	case req.Method == http.MethodPost && allMenuRegex.MatchString(req.URL.Path):
+		c.PostMenu(rw, req)
+
+	// case req.Method == http.MethodGet && allChefsRegex.MatchString(req.URL.Path):
+	// 	c.GetChef(rw, req)
+
+	// case req.Method == http.MethodGet && specificChefRegex.MatchString(req.URL.Path):
+	// 	c.GetChefByName(rw, req)
+
+	// case req.Method == http.MethodDelete && allChefsRegex.MatchString(req.URL.Path):
+	// 	c.DeleteChef(rw, req)
+
+	// case req.Method == http.MethodDelete && specificChefRegex.MatchString(req.URL.Path):
+	// 	c.DeleteChefByName(rw, req)
+
+	default:
+		c.notFound(rw, req) // returns 501 Not Implemented
+	}
 
 }
