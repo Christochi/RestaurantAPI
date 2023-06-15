4be1483c560074a3ad63caad7b9bede5f8898498
Christochi <tochukwui2@gmail.com>
2023-06-15 04:52:03 +0000

    comment

--- api/menu/menu.go
+++ api/menu/menu.go
@@ -202,7 +202,7 @@ func (m *menu) DeleteMeal(rw http.ResponseWriter, req *http.Request) {
 
 		// delete an element
 		if value.Meal == meal {
-			(*m)[index] = (*m)[len(*m)-1]
+			(*m)[index] = (*m)[len(*m)-1] // replace the element with the last element
 			*m = (*m)[:len(*m)-1]
 		}
 
