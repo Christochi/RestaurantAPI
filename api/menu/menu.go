909260a0cc9fe8b193e0c1cd0a761ef919efa96f
Christochi <tochukwui2@gmail.com>
2023-06-19 00:57:21 +0000

    refactored getMealType

--- api/menu/menu.go
+++ api/menu/menu.go
@@ -11,12 +11,13 @@ import (
 
 // pathnames for subroot in url endpoint
 var (
-	allMenuRegex      = regexp.MustCompile(`^\/menu[\/]?$`)                                          // /menu or /menu/
-	allBreakfastRegex = regexp.MustCompile(`^\/menu\/(breakfast)$`)                                  // /menu/breakfast
-	allLunchRegex     = regexp.MustCompile(`^\/menu\/(lunch)$`)                                      // /menu/lunch
-	allDinnerRegex    = regexp.MustCompile(`^\/menu\/(dinner)$`)                                     // /menu/dinner
-	allDrinksRegex    = regexp.MustCompile(`^\/menu\/(drinks)$`)                                     // /menu/drinks
-	mealRegex         = regexp.MustCompile(`^\/menu\/(breakfast|lunch|drinks|dinner)\/([A-Za-z]+)$`) // /menu/<anymealtype>/burger
+	allMenuRegex = regexp.MustCompile(`^\/menu[\/]?$`) // /menu or /menu/
+	//allBreakfastRegex = regexp.MustCompile(`^\/menu\/(breakfast)$`)                                  // /menu/breakfast
+	//allLunchRegex     = regexp.MustCompile(`^\/menu\/(lunch)$`)                                      // /menu/lunch
+	//allDinnerRegex    = regexp.MustCompile(`^\/menu\/(dinner)$`)                                     // /menu/dinner
+	//allDrinksRegex    = regexp.MustCompile(`^\/menu\/(drinks)$`)                                     // /menu/drinks
+	mealRegex = regexp.MustCompile(`^\/menu\/(breakfast|lunch|drinks|dinner)\/([A-Za-z]+)$`) // /menu/<anymealtype>/burger
+	mealType  = regexp.MustCompile(`^\/menu\/([A-Za-z]+)$`)
 )
 
 // Menu json Object
@@ -51,17 +52,20 @@ func (m *menu) MenuHandler(rw http.ResponseWriter, req *http.Request) {
 	case req.Method == http.MethodGet && allMenuRegex.MatchString(req.URL.Path):
 		m.GetMenu(rw, req)
 
-	case req.Method == http.MethodGet && allBreakfastRegex.MatchString(req.URL.Path):
-		m.GetBreakfastMenu(rw, req)
+	case req.Method == http.MethodGet && mealType.MatchString(req.URL.Path):
+		m.getMealType(rw, req)
 
-	case req.Method == http.MethodGet && allLunchRegex.MatchString(req.URL.Path):
-		m.GetLunchMenu(rw, req)
+	// case req.Method == http.MethodGet && allBreakfastRegex.MatchString(req.URL.Path):
+	// 	m.GetBreakfastMenu(rw, req)
 
-	case req.Method == http.MethodGet && allDinnerRegex.MatchString(req.URL.Path):
-		m.GetDinnerMenu(rw, req)
+	// case req.Method == http.MethodGet && allLunchRegex.MatchString(req.URL.Path):
+	// 	m.GetLunchMenu(rw, req)
 
-	case req.Method == http.MethodGet && allDrinksRegex.MatchString(req.URL.Path):
-		m.GetDrinksMenu(rw, req)
+	// case req.Method == http.MethodGet && allDinnerRegex.MatchString(req.URL.Path):
+	// 	m.GetDinnerMenu(rw, req)
+
+	// case req.Method == http.MethodGet && allDrinksRegex.MatchString(req.URL.Path):
+	// 	m.GetDrinksMenu(rw, req)
 
 	case req.Method == http.MethodDelete && allMenuRegex.MatchString(req.URL.Path):
 		m.DeleteMenu(rw, req)
@@ -105,75 +109,83 @@ func (m *menu) GetMenu(rw http.ResponseWriter, req *http.Request) {
 }
 
 // gets the list of menu for a meal type
-func getMealType(m *menu, rw http.ResponseWriter, req *http.Request, urlSubPaths []string) {
+func (m *menu) getMealType(rw http.ResponseWriter, req *http.Request) {
+
+	// returns slice of substrings that matches subexpressions in the url
+	urlSubPaths := mealType.FindStringSubmatch(req.URL.Path)
 
 	// since the order of the slice is known, store the second index
 	// example: /menu/breakfast = ["/menu/breakfast", "breakfast"]
-	mealType := urlSubPaths[1]
+	mealTypeName := urlSubPaths[1]
 
 	var meal []menuJson // new slice to hold the filtered data
 
 	for _, value := range *m {
 
-		if strings.ToLower(value.Type) == mealType {
+		if strings.ToLower(value.Type) == mealTypeName {
 			meal = append(meal, value) // append to new slice
+
+			// encode to json and rw sends the json
+			err := json.NewEncoder(rw).Encode(meal)
+
+			// error handling
+			if err != nil {
+				log.Fatal("error encoding into json")
+			}
+
+			return
 		}
 
 	}
 
-	// encode to json and rw sends the json
-	err := json.NewEncoder(rw).Encode(meal)
-
-	// error handling
-	if err != nil {
-		log.Fatal("error encoding into json")
-	}
+	rw.WriteHeader(http.StatusNotFound)                    // 404
+	rw.Write([]byte(http.StatusText(http.StatusNotFound))) // NotFound
 
 }
 
 // client requests for breakfast menu
-func (m *menu) GetBreakfastMenu(rw http.ResponseWriter, req *http.Request) {
+// func (m *menu) GetBreakfastMenu(rw http.ResponseWriter, req *http.Request) {
 
-	// returns slice of substrings that matches subexpressions in the url
-	urlSubPaths := allBreakfastRegex.FindStringSubmatch(req.URL.Path)
+// 	// returns slice of substrings that matches subexpressions in the url
+// 	urlSubPaths := allBreakfastRegex.FindStringSubmatch(req.URL.Path)
 
-	// gets the list of menu for a meal type
-	getMealType(m, rw, req, urlSubPaths)
+// 	// gets the list of menu for a meal type
+// 	getMealType(m, rw, req, urlSubPaths)
 
-}
+// }
 
-// client requests for lunch menu
-func (m *menu) GetLunchMenu(rw http.ResponseWriter, req *http.Request) {
+// // client requests for lunch menu
+// func (m *menu) GetLunchMenu(rw http.ResponseWriter, req *http.Request) {
 
-	// returns slice of substrings that matches subexpressions in the url
-	urlSubPaths := allLunchRegex.FindStringSubmatch(req.URL.Path)
+// 	// returns slice of substrings that matches subexpressions in the url
+// 	urlSubPaths := allLunchRegex.FindStringSubmatch(req.URL.Path)
 
-	// gets the list of menu for a meal type
-	getMealType(m, rw, req, urlSubPaths)
+// 	// gets the list of menu for a meal type
+// 	getMealType(m, rw, req, urlSubPaths)
 
-}
+// }
 
-// client requests for dinner menu
-func (m *menu) GetDinnerMenu(rw http.ResponseWriter, req *http.Request) {
+// // client requests for dinner menu
+// func (m *menu) GetDinnerMenu(rw http.ResponseWriter, req *http.Request) {
 
-	// returns slice of substrings that matches subexpressions in the url
-	urlSubPaths := allDinnerRegex.FindStringSubmatch(req.URL.Path)
+// 	// returns slice of substrings that matches subexpressions in the url
+// 	urlSubPaths := allDinnerRegex.FindStringSubmatch(req.URL.Path)
 
-	// gets the list of menu for a meal type
-	getMealType(m, rw, req, urlSubPaths)
+// 	// gets the list of menu for a meal type
+// 	getMealType(m, rw, req, urlSubPaths)
 
-}
+// }
 
-// client requests for drinks menu
-func (m *menu) GetDrinksMenu(rw http.ResponseWriter, req *http.Request) {
+// // client requests for drinks menu
+// func (m *menu) GetDrinksMenu(rw http.ResponseWriter, req *http.Request) {
 
-	// returns slice of substrings that matches subexpressions in the url
-	urlSubPaths := allDrinksRegex.FindStringSubmatch(req.URL.Path)
+// 	// returns slice of substrings that matches subexpressions in the url
+// 	urlSubPaths := allDrinksRegex.FindStringSubmatch(req.URL.Path)
 
-	// gets the list of menu for a meal type
-	getMealType(m, rw, req, urlSubPaths)
+// 	// gets the list of menu for a meal type
+// 	getMealType(m, rw, req, urlSubPaths)
 
-}
+// }
 
 // client deletes all menu data
 func (m *menu) DeleteMenu(rw http.ResponseWriter, req *http.Request) {
