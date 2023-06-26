a5d061141e59eb08d3df2e9401abf6d0ddff9207
Christochi <tochukwui2@gmail.com>
2023-06-26 16:49:50 +0000

    variable initialization

--- api/reservation/reservation.go
+++ api/reservation/reservation.go
@@ -1,1 +1,9 @@
 package reservation
+
+import "regexp"
+
+// pathnames for subroot in url endpoint
+var (
+	reservationRegex = regexp.MustCompile(`^\/reservation[\/]?$`)         // /reservation or /reservation/
+	customerRegex    = regexp.MustCompile(`^\/reservation\/([A-Za-z]+)$`) // /reservation/<customername> => /reservation/henryduru
+)
