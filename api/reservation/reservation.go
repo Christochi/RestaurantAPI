738cff89028e259e9ad5b431c51e78aadc147c3d
Christochi <tochukwui2@gmail.com>
2023-06-27 03:58:46 +0000

    not ready foro implementation

--- api/reservation/reservation.go
+++
@@ -1,7 +0,0 @@
-package reservation
-
-// pathnames for subroot in url endpoint
-// var (
-// 	reservationRegex = regexp.MustCompile(`^\/reservation[\/]?$`)         // /reservation or /reservation/
-// 	customerRegex    = regexp.MustCompile(`^\/reservation\/([A-Za-z]+)$`) // /reservation/<customername> => /reservation/henryduru
-// )
