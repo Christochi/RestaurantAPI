02177ace2e8f1e108c631ee804abbb625653c7a0
Christochi <tochukwui2@gmail.com>
2023-06-30 16:54:09 +0000

    removed image field

--- api/chef/chef.go
+++ api/chef/chef.go
@@ -20,7 +20,6 @@ var (
 type chefJson struct {
 	Name  string `json:"name"`
 	About string `json:"about"`
-	Image string `json:"image"`
 }
 
 type chef []chefJson // slice type to be used as a receiver for methods
