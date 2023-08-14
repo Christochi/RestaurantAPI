d21342f023ab0fcb17bf3d0a81a10d4b8fcfa92e
Christochi <tochukwui2@gmail.com>
2023-08-14 01:52:13 +0000

    status code test

--- webserver/server_test.go
+++ webserver/server_test.go
@@ -3,93 +3,126 @@
 package webserver
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
-	"restaurantapi/api/chef"
-	"restaurantapi/api/menu"
 	"testing"
 )
 
-func TestChefHandler(t *testing.T) {
+func TestFileServer(t *testing.T) {
 
-	t.Parallel()
-
-	newChef := chef.NewChef() // chef object
+	// returns a handler that serves HTTP request with file contents
+	fileHandler := http.FileServer(http.Dir("../static"))
 
 	//starts and returns a server
-	server := httptest.NewServer(http.HandlerFunc(newChef.ChefHandler))
+	server := httptest.NewServer(noCache(http.StripPrefix("/", fileHandler)))
+	defer server.Close()
 
-	url := server.URL + "/chef" // url
-
-	// creates a request to query the server
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	// sends a request to the URL
+	resp, err := http.Get(server.URL)
 	if err != nil {
 		t.Error(err)
 	}
 
-	// send the HTTP request and return an HTTP response
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Error(err)
+	statusCode := resp.StatusCode
+	if expectedStatusCode := http.StatusOK; statusCode != expectedStatusCode {
+		t.Errorf("want %d, got %d", expectedStatusCode, statusCode)
 	}
 
-	statusCode := resp.StatusCode // return status code gotten after hitting the endpoint
-	want := 200
-
-	if statusCode != want {
-		t.Errorf("want %d, got %d", want, statusCode)
-	}
+	fmt.Printf("%#v\n", resp)
 
 	// defer resp.Body.Close()
 	// body, err := io.ReadAll(resp.Body) // get the response body
 	// if err != nil {
-	// 	t.Fatal(err)
+	// 	t.Error(err)
 	// }
 
-	// if body != nil {
-	// 	t.Errorf("want nil, got %v", body)
-	// }
-
-}
-
-func TestMenuHandler(t *testing.T) {
-
-	t.Parallel()
-
-	newMenu := menu.NewMenu() // menu object
-
-	//starts and returns a server
-	server := httptest.NewServer(http.HandlerFunc(newMenu.MenuHandler))
-
-	url := server.URL + "/menu" // url
-
-	// creates a request to query the server
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		t.Error(err)
-	}
-
-	// send the HTTP request and return an HTTP response
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		t.Error(err)
-	}
-
-	statusCode := resp.StatusCode // return status code gotten after hitting the endpoint
-	want := 200
-
-	if statusCode != want {
-		t.Errorf("want %d, got %d", want, statusCode)
-	}
-
-	// defer resp.Body.Close()
-	// body, err := io.ReadAll(resp.Body) // get the response body
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-
-	// if body != nil {
-	// 	t.Errorf("want nil, got %v", body)
+	// if body == nil {
+	// 	t.Errorf("want nil, got %v", string(body))
 	// }
 
 }
+
+// func TestChefHandler(t *testing.T) {
+
+// 	t.Parallel()
+
+// 	newChef := chef.NewChef() // chef object
+
+// 	//starts and returns a server
+// 	server := httptest.NewServer(http.HandlerFunc(newChef.ChefHandler))
+
+// 	url := server.URL + "/chef" // url
+
+// 	// creates a request to query the server
+// 	req, err := http.NewRequest(http.MethodGet, url, nil)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	// send the HTTP request and return an HTTP response
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	statusCode := resp.StatusCode // return status code gotten after hitting the endpoint
+// 	want := 200
+
+// 	if statusCode != want {
+// 		t.Errorf("want %d, got %d", want, statusCode)
+// 	}
+
+// 	// defer resp.Body.Close()
+// 	// body, err := io.ReadAll(resp.Body) // get the response body
+// 	// if err != nil {
+// 	// 	t.Fatal(err)
+// 	// }
+
+// 	// if body != nil {
+// 	// 	t.Errorf("want nil, got %v", body)
+// 	// }
+
+// }
+
+// func TestMenuHandler(t *testing.T) {
+
+// 	t.Parallel()
+
+// 	newMenu := menu.NewMenu() // menu object
+
+// 	//starts and returns a server
+// 	server := httptest.NewServer(http.HandlerFunc(newMenu.MenuHandler))
+
+// 	url := server.URL + "/menu" // url
+
+// 	// creates a request to query the server
+// 	req, err := http.NewRequest(http.MethodGet, url, nil)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	// send the HTTP request and return an HTTP response
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+
+// 	statusCode := resp.StatusCode // return status code gotten after hitting the endpoint
+// 	want := 200
+
+// 	if statusCode != want {
+// 		t.Errorf("want %d, got %d", want, statusCode)
+// 	}
+
+// 	// defer resp.Body.Close()
+// 	// body, err := io.ReadAll(resp.Body) // get the response body
+// 	// if err != nil {
+// 	// 	t.Fatal(err)
+// 	// }
+
+// 	// if body != nil {
+// 	// 	t.Errorf("want nil, got %v", body)
+// 	// }
+
+// }
