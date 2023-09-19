321579acfdf32a35d309606887fa87e0893e348a
Christochi <tochukwui2@gmail.com>
2023-09-19 04:17:55 +0000

    db conn code

--- database/dbconn.go
+++ database/dbconn.go
@@ -1,7 +1,34 @@
 package database
 
-import _ "github.com/jackc/pgx/v5/stdlib"
+import (
+	"database/sql"
+	"fmt"
+	"net/url"
 
-func conn() {
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
 
+func conn() *sql.DB {
+
+	// construct db url
+	dsn := url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword("postgres", "password"),
+		Host:   "localhost:5432",
+		Path:   "postgres", // dbname (POSTGRES_DB)
+	}
+
+	urlValues := dsn.Query()            //Query parses RawQuery and returns the corresponding values
+	urlValues.Add("sslmode", "disable") // disbale sslmode and add to the map of Values
+
+	// database url : postgres://postgres:password@localhost:5432/postgres?sslmode=disable
+	dsn.RawQuery = urlValues.Encode()
+
+	// create db connection
+	db, err := sql.Open("pgx", dsn.String())
+	if err != nil {
+		fmt.Println("sql open", err)
+	}
+
+	return db
 }
