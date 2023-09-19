b9d230012f2d553bc158f8b53c686a9f10c05a09
Christochi <tochukwui2@gmail.com>
2023-09-19 04:24:22 +0000

    db conn test file

---
+++ database/dbconn_test.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"fmt"
+	"testing"
+)
+
+// Test DB Connection
+func TestConn(t *testing.T) {
+
+	connTest := conn()     // returns the database
+	err := connTest.Ping() // ping db
+	if err != nil {
+		if err != nil {
+			fmt.Println("test conn", err)
+		}
+	}
+}
