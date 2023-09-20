8172aaf8bc7ad7a12bcbe547414c41d0b2358049
Christochi <tochukwui2@gmail.com>
2023-09-20 02:38:59 +0000

    interpolation of env vars

--- database/dbconn.go
+++ database/dbconn.go
@@ -3,25 +3,34 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/url"
+	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 )
 
 func conn() *sql.DB {
 
-	// construct db url
-	dsn := url.URL{
-		Scheme: "postgres",
-		User:   url.UserPassword("postgres", "password"),
-		Host:   "localhost:5432",
-		Path:   "postgres", // dbname (POSTGRES_DB)
+	// load environment variables
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
-	urlValues := dsn.Query()            //Query parses RawQuery and returns the corresponding values
-	urlValues.Add("sslmode", "disable") // disbale sslmode and add to the map of Values
+	// construct db url
+	dsn := url.URL{
+		Scheme: os.Getenv("URL_SCHEME"),
+		User:   url.UserPassword(os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD")),
+		Host:   "localhost:5432",
+		Path:   os.Getenv("POSTGRES_DB"),
+	}
 
-	// database url : postgres://postgres:password@localhost:5432/postgres?sslmode=disable
+	urlValues := dsn.Query()                       //Query parses RawQuery and returns the corresponding values
+	urlValues.Add(os.Getenv("SSLMODE"), "disable") // disbale sslmode and add to the map of Values
+
+	// database url = URL_SCHEME://POSTGRES_USER:POSTGRES_PASSWORD@HOST:PORT/POSTGRES_DB?sslmode=SSLMODE
 	dsn.RawQuery = urlValues.Encode()
 
 	// create db connection
