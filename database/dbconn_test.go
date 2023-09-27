1bb4dc41d214fab29e12a23af3f48d007d336cfc
Christochi <tochukwui2@gmail.com>
2023-09-27 02:51:35 +0000

    db connection test file

--- database/dbconn_test.go
+++ database/dbconn_test.go
@@ -2,15 +2,26 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"testing"
+
+	"github.com/joho/godotenv"
 )
 
 // Test DB Connection
 func TestConn(t *testing.T) {
 
-	connTest := conn()     // returns the database
-	err := connTest.Ping() // ping db
+	// load environment variables
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	connTest := conn()    // returns the database
+	err = connTest.Ping() // ping db
 	if err != nil {
 		fmt.Println("test conn", err)
 	}
+
+	defer connTest.Close() // close db
 }
