854302136fcb2b254362983f393f0cc55f629711
Christochi <tochukwui2@gmail.com>
2023-10-01 06:06:42 +0000

    openfile and exec... as utitlity

--- utils/dbUtils.go
+++ utils/dbUtils.go
@@ -1,1 +1,35 @@
 package utils
+
+import (
+	"database/sql"
+	"fmt"
+	"io"
+	"os"
+)
+
+// open and execute SQL script
+func OpenFile(filename string, db *sql.DB) {
+
+	file, err := os.Open(filename)
+	if err == nil {
+		panic(err)
+	}
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	execSQLScript(content, db)
+
+}
+
+func execSQLScript(content []byte, db *sql.DB) {
+
+	_, err := db.Exec(string(content))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
