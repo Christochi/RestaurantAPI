808e17059c9cd81eac2a6f8c2ae26faaa4e21424
Christochi <tochukwui2@gmail.com>
2023-10-01 06:06:06 +0000

    db set up

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
+	"restaurantapi/database"
+	"restaurantapi/utils"
 	"restaurantapi/webserver"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -9,7 +14,21 @@ func main() {
 	// directory of web files
 	webserver.WebFilesDir = "./static"
 
+	// connect to database and create db tables
+	dbConn()
+
 	// start the server
 	webserver.RunServer(webserver.WebFilesDir)
 
+}
+
+func dbConn() {
+
+	// load environment variables
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db := database.Conn()                // establish db connections and return the database
+	utils.OpenFile("chef_table.sql", db) // read and create SQL table
 }
