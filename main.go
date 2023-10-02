c633ba2ba044863d0b5e45760842ef7e2a8a05fb
Christochi <tochukwui2@gmail.com>
2023-10-02 04:39:13 +0000

    reordered execution of code

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ import (
 
 func main() {
 
-	// directory of web files
-	webserver.WebFilesDir = "./static"
-
 	// connect to database and create db tables
 	dbConn()
+
+	// directory of web files
+	webserver.WebFilesDir = "./static"
 
 	// start the server
 	webserver.RunServer(webserver.WebFilesDir)
@@ -29,6 +29,6 @@ func dbConn() {
 		log.Fatal("Error loading .env file")
 	}
 
-	db := database.Conn()                // establish db connections and return the database
-	utils.OpenFile("chef_table.sql", db) // read and create SQL table
+	db := database.Conn()                   // establish db connections and return the database
+	utils.OpenFile("create_tables.sql", db) // read and create SQL table
 }
