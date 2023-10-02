6fb9445109d92ccd62b5f373805b18680f5ea525
Christochi <tochukwui2@gmail.com>
2023-10-02 05:32:16 +0000

    sql script for table creation

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,7 +1,13 @@
+-- DROP TABLES
+DROP TABLE IF EXISTS Chef;
+DROP TABLE IF EXISTS Menu;
+
 -- CREATE Chef Table
 CREATE TABLE Chef (
-   ID int,
-   Name  varchar,
-   Image varchar,
-   Age int
+   id SERIAL PRIMARY KEY,
+   full_name  VARCHAR(50),
+   about VARCHAR(255),
+   image_name VARCHAR(50),
+   gender VARCHAR(1), -- "M"/"F" (male/female)
+   age int
 );
