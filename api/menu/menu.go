a62efa802bb8c76e2c27ac2a2f1cf4775577f66d
Christochi <tochukwui2@gmail.com>
2023-10-10 02:46:07 +0000

    refactored getMenu, GetMealType, DeleteMenu

--- api/menu/menu.go
+++ api/menu/menu.go
@@ -54,14 +54,14 @@ func (m *menu) MenuHandler(rw http.ResponseWriter, req *http.Request) {
 	case req.Method == http.MethodGet && mealTypeRegex.MatchString(req.URL.Path):
 		m.getMealType(rw, req)
 
-	case req.Method == http.MethodGet && mealRegex.MatchString(req.URL.Path):
-		m.getMeal(rw, req)
+	// case req.Method == http.MethodGet && mealRegex.MatchString(req.URL.Path):
+	// 	m.getMeal(rw, req)
 
 	case req.Method == http.MethodDelete && allMenuRegex.MatchString(req.URL.Path):
 		m.deleteMenu(rw)
 
-	case req.Method == http.MethodDelete && mealRegex.MatchString(req.URL.Path):
-		m.deleteMeal(rw, req)
+	// case req.Method == http.MethodDelete && mealRegex.MatchString(req.URL.Path):
+	// 	m.deleteMeal(rw, req)
 
 	default:
 		utils.ServerMessage(rw, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented) // returns 501 Not Implemented
@@ -128,7 +128,7 @@ func (m *menu) getMenu(rw http.ResponseWriter) {
 	var column menuJson // placeholder for column values
 
 	// get the rows from db
-	rows := utils.SelectRows(utils.SelectAllMenuRowsQuery, utils.Database)
+	rows := utils.SelectRows(utils.SelectAllMenuQuery, utils.Database)
 	m.iterDBRows(rows, column)
 
 	// read and encode to json
@@ -138,6 +138,9 @@ func (m *menu) getMenu(rw http.ResponseWriter) {
 
 // gets the list of menu for a meal type
 func (m *menu) getMealType(rw http.ResponseWriter, req *http.Request) {
+
+	// initialize to nil to clear any initial value so that fresh copy of the data in db can be stored
+	*m = nil
 
 	// returns slice of substrings that matches subexpressions in the url
 	urlSubPaths := mealTypeRegex.FindStringSubmatch(req.URL.Path)
@@ -149,65 +152,68 @@ func (m *menu) getMealType(rw http.ResponseWriter, req *http.Request) {
 	// log for informational purpose
 	requestLogger.Printf("GET meal type request at /menu/%s endpoint", mealTypeName)
 
-	var meal []menuJson // new slice to hold the filtered data
+	var column menuJson // placeholder for column values
 
-	for _, value := range *m {
+	// Retrieve data that matches the substring from the db
+	rows := utils.SelectRows(utils.SelectMealTypeQuery, utils.Database, mealTypeName)
+	m.iterDBRows(rows, column)
 
-		if strings.ToLower(value.Type) == mealTypeName {
-			meal = append(meal, value) // append to new slice
-		}
-
-	}
-
-	if meal == nil {
+	if *m == nil {
 		utils.ServerMessage(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound) // 404 Not Found
 
 		return // exit function call
 	}
 
 	// read and encode to json
-	utils.Get(rw, meal)
+	utils.Get(rw, m)
 
 }
 
 // gets a specific meal
-func (m *menu) getMeal(rw http.ResponseWriter, req *http.Request) {
+// func (m *menu) getMeal(rw http.ResponseWriter, req *http.Request) {
 
-	// returns slice of substrings that matches subexpressions in the url
-	urlSubPaths := mealRegex.FindStringSubmatch(req.URL.Path)
+// 	// initialize to nil to clear any initial value so that fresh copy of the data in db can be stored
+// 	//*m = nil
 
-	// since the order of the slice is known, store the second index
-	// example: /menu/<mealtype>/<mealname> = ["/menu/drinks/mangolasse", "drinks", "mangolasse"]
-	mealTypeName := strings.ToLower(urlSubPaths[1])
-	mealName := strings.ToLower(urlSubPaths[2])
+// 	// returns slice of substrings that matches subexpressions in the url
+// 	urlSubPaths := mealRegex.FindStringSubmatch(req.URL.Path)
 
-	// log for informational purpose
-	requestLogger.Printf("GET meal request at /menu/%s/%s endpoint", mealTypeName, mealName)
+// 	// since the order of the slice is known, store the second index
+// 	// example: /menu/<mealtype>/<mealname> = ["/menu/drinks/mangolasse", "drinks", "mangolasse"]
+// 	mealTypeName := strings.ToLower(urlSubPaths[1])
+// 	mealName := strings.ToLower(urlSubPaths[2])
 
-	var meal []menuJson // new slice to hold the filtered data
+// 	// log for informational purpose
+// 	requestLogger.Printf("GET meal request at /menu/%s/%s endpoint", mealTypeName, mealName)
 
-	for _, value := range *m {
+// 	// var meal []menuJson // new slice to hold the filtered data
+// 	var column menuJson // placeholder for column values
 
-		// remove whitespaces and returns lower case of the string
-		name := strings.ToLower(strings.ReplaceAll(value.Meal, " ", ""))
+// 	// Retrieve data that matches the substring from the db
+// 	rows := utils.SelectRows(utils.SelectMealQuery, utils.Database, "%"+mealName+"%")
+// 	m.iterDBRows(rows, column)
+// 	// for _, value := range *m {
 
-		// compares if 2 strings have the same string literal or a substring
-		if name == mealName || strings.Contains(name, mealName) {
-			meal = append(meal, value) // append to new slice
-		}
+// 	// 	// remove whitespaces and returns lower case of the string
+// 	// 	name := strings.ToLower(strings.ReplaceAll(value.Meal, " ", ""))
 
-	}
+// 	// 	// compares if 2 strings have the same string literal or a substring
+// 	// 	if name == mealName || strings.Contains(name, mealName) {
+// 	// 		meal = append(meal, value) // append to new slice
+// 	// 	}
 
-	if meal == nil {
-		utils.ServerMessage(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound) // 404 Not Found
+// 	// }
 
-		return // exit function call
-	}
+// 	if *m == nil {
+// 		utils.ServerMessage(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound) // 404 Not Found
 
-	// read and encode to json
-	utils.Get(rw, meal)
+// 		return // exit function call
+// 	}
 
-}
+// 	// read and encode to json
+// 	utils.Get(rw, m)
+
+// }
 
 // client deletes all menu data
 func (m *menu) deleteMenu(rw http.ResponseWriter) {
@@ -218,39 +224,42 @@ func (m *menu) deleteMenu(rw http.ResponseWriter) {
 	// delete all element by re-initializing to nil
 	*m = nil
 
-	//utils.Delete(rw, m)
+	// Delete all rows from the menu table and reset PK to 1
+	utils.ExecuteQueries(utils.DeleteMenuRowsQuery, utils.Database)
+
+	utils.ServerMessage(rw, "resource deleted successfully", http.StatusOK) // 200 OK
 
 }
 
 // deletes a specific meal
-func (m *menu) deleteMeal(rw http.ResponseWriter, req *http.Request) {
+// func (m *menu) deleteMeal(rw http.ResponseWriter, req *http.Request) {
 
-	// returns slice of substrings that matches subexpressions in the url
-	urlSubPaths := mealRegex.FindStringSubmatch(req.URL.Path)
+// 	// returns slice of substrings that matches subexpressions in the url
+// 	urlSubPaths := mealRegex.FindStringSubmatch(req.URL.Path)
 
-	// since the order of the slice is known, store the third index
-	// example: /menu/<mealtype>/<mealname> = ["/menu/lunch/burger", "lunch", "burger"]
-	mealTypeName := strings.ToLower(urlSubPaths[1])
-	meal := strings.ToLower(urlSubPaths[2])
+// 	// since the order of the slice is known, store the third index
+// 	// example: /menu/<mealtype>/<mealname> = ["/menu/lunch/burger", "lunch", "burger"]
+// 	mealTypeName := strings.ToLower(urlSubPaths[1])
+// 	meal := strings.ToLower(urlSubPaths[2])
 
-	// log for informational purpose
-	requestLogger.Printf("DELETE meal request at /menu/%s/%s endpoint", mealTypeName, meal)
+// 	// log for informational purpose
+// 	requestLogger.Printf("DELETE meal request at /menu/%s/%s endpoint", mealTypeName, meal)
 
-	for index, value := range *m {
+// 	// for index, value := range *m {
 
-		// remove whitespaces and returns lower case of the string
-		if strings.ToLower(strings.ReplaceAll(value.Meal, " ", "")) == meal {
-			// delete an element
-			(*m)[index] = (*m)[len(*m)-1] // replace the element with the last element
-			*m = (*m)[:len(*m)-1]         // reinitialize the array with all the elements excluding last element
+// 	// 	// remove whitespaces and returns lower case of the string
+// 	// 	if strings.ToLower(strings.ReplaceAll(value.Meal, " ", "")) == meal {
+// 	// 		// delete an element
+// 	// 		(*m)[index] = (*m)[len(*m)-1] // replace the element with the last element
+// 	// 		*m = (*m)[:len(*m)-1]         // reinitialize the array with all the elements excluding last element
 
-			utils.ServerMessage(rw, "resource deleted successfully", http.StatusOK) // 200 OK
+// 	// 		utils.ServerMessage(rw, "resource deleted successfully", http.StatusOK) // 200 OK
 
-			return // exit function call
-		}
+// 	// 		return // exit function call
+// 	// 	}
 
-	}
+// 	// }
 
-	utils.ServerMessage(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound) // 404 Not Found
+// 	utils.ServerMessage(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound) // 404 Not Found
 
-}
+// }
