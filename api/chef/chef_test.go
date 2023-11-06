bb7ecd916d3346b64b8fcd42fbfec3a1e144a5de
Christochi <tochukwui2@gmail.com>
2023-11-06 18:17:47 +0000

    post chef unit test

---
+++ api/chef/chef_test.go
@@ -0,0 +1,59 @@
+// Testing Chef API
+
+package chef
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// Test data is decoded to struct
+func TestPostChef(t *testing.T) {
+
+	t.Parallel()
+
+	chef := NewChef()      // chef object
+	b := new(bytes.Buffer) // zero value of buffer of bytes
+
+	// test data
+	testData := []chefJson{
+		{
+			Name:   "Chocho Okoye",
+			About:  "Assistant Chef",
+			Image:  "chocho.jpg",
+			Gender: "M",
+			Age:    2,
+		},
+		{
+			Name:   "John Abraham",
+			About:  "John Abraham has 7 years experience making delicious meals for 5-star hotels and famous restaurants in North America",
+			Image:  "johnabraham.jpg",
+			Gender: "M",
+			Age:    35,
+		},
+	}
+
+	// encode to json
+	_ = json.NewEncoder(b).Encode(testData)
+
+	// captures everything that is written with the ResponseWriter and returns ResponseRecorder
+	rec := httptest.NewRecorder()
+
+	// creates a request
+	req := httptest.NewRequest(http.MethodPost, "/chef", b)
+
+	chef.ChefHandler(rec, req) // call postChef
+
+	for i, c := range *chef {
+		for k, data := range testData {
+
+			if i == k && c != data {
+				t.Errorf("want %#v\n, got  %#v\n", c, data)
+			}
+		}
+	}
+
+}
