0aeca2eb2f70066679f55429ca977ae0c1e18647
Christochi <tochukwui2@gmail.com>
2023-12-10 05:52:26 +0000

    custom api error

---
+++ errors/apierror.go
@@ -0,0 +1,34 @@
+package errror
+
+import (
+	"errors"
+	"net/http"
+	"restaurantapi/utils"
+	"strconv"
+
+	"github.com/Christochi/error-handler/service"
+)
+
+var requestLogger = utils.InfoLog() // return info field
+
+type apiError struct {
+	err    string
+	status int
+}
+
+func RestError(rw http.ResponseWriter, err error) {
+	var apierr apiError
+	var svcErr *service.ServiceError
+
+	if errors.As(err, &svcErr) {
+		apierr.err = svcErr.AppError().Error()
+		var atoiErr error
+		apierr.status, atoiErr = strconv.Atoi(svcErr.ErrDesc())
+		if atoiErr != nil {
+			requestLogger.Println(atoiErr)
+		}
+	}
+
+	http.Error(rw, apierr.err, apierr.status)
+
+}
