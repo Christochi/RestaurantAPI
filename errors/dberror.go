607a1c35b66c23db040773d5a4ad5216e390cc91
Christochi <tochukwui2@gmail.com>
2023-12-12 00:14:22 +0000

    file for db custom error

--- errors/dberror.go
+++ errors/dberror.go
@@ -1,11 +1,31 @@
 package errors
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Christochi/error-handler/service"
+)
+
 type dbError struct {
 	err     string
 	message string
 }
 
+func (db *dbError) Error() string {
+	return fmt.Sprintf("DB Error: %v -- %v", db.err, db.message)
+}
+
 // Custom DB Error
-func DatabaseError(errs error) {
+func DatabaseError(errs error) *dbError {
+	var dberr dbError
+	var svcErr *service.ServiceError
+
+	if errors.As(errs, &svcErr) {
+		dberr.err = svcErr.AppError().Error()
+		dberr.message = svcErr.ErrDesc()
+	}
+
+	return &dberr
 
 }
